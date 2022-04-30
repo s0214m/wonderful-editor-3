805f9ae949bfb0b14468ff7998ef8aacc21330e1
s0214m <m.suguru.0214@gmail.com>
2022-04-30 15:03:45 +0900

    alias_methodでヘルパーメソッド名を変更

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::BaseApiController < ApplicationController
-  def current_user
-    @current_user ||= User.first
-  end
+  before_action :authenticate_user!
+
+  alias_method :current_user, :current_api_v1_user
+  alias_method :user_signed_in?, :api_v1_user_signed_in?
+  alias_method :authenticate_user!, :authenticate_api_v1_user!
 end
